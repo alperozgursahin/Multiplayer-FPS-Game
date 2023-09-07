c30b8c5d04cf4ed57aa13f2b973152f3c7fd685b
Alper Ozgur Sahin <alperozgursahin6@gmail.com>
2023-09-07 22:37:31 +0300

    Crosshair
    Disable player graphics because armor shows on camera and its annoying and added crosshair

--- Multiplayer FPS Game/Assets/Scripts/PlayerSetup.cs
+++ Multiplayer FPS Game/Assets/Scripts/PlayerSetup.cs
@@ -14,6 +14,17 @@ public class PlayerSetup : NetworkBehaviour
     [SerializeField]
     Behaviour audioListener;
 
+    [SerializeField]
+    string dontDrawLayerName = "DontDraw";
+
+    [SerializeField]
+    GameObject playerGraphics;
+
+    [SerializeField]
+    GameObject playerUIPrefab;
+    private GameObject playerUIInstance;
+
+
     Camera sceneCamera;
 
     void Start()
@@ -34,10 +45,27 @@ public class PlayerSetup : NetworkBehaviour
                 sceneCamera.gameObject.SetActive(false);
             }
             audioListener.gameObject.SetActive(false);
+            
+            SetLayerRecursively (playerGraphics, LayerMask.NameToLayer(dontDrawLayerName));
+
+            // Create PlayerUI
+            playerUIInstance = Instantiate(playerUIPrefab);
+            playerUIInstance.name = playerUIPrefab.name;
+
         }
 
         GetComponent<Player>().Setup();
 
+    }
+
+    void SetLayerRecursively(GameObject obj, int newLayer)
+    {
+        obj.layer = newLayer;
+
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, newLayer);
+        }
     }
 
     public override void OnStartClient()
@@ -66,6 +94,8 @@ public class PlayerSetup : NetworkBehaviour
     // When we are destroyed
     void OnDisable()
     {
+        Destroy(playerUIInstance);
+
         // Re-enable the scene camera
         if (sceneCamera != null)
         {
